f702cb22dc544dc95eefbf656e7fa0a0b3a2cc42
huangcy <huangcy@autoai.com>
2022-03-01 18:46:53 +0800

    update httpd.c

--- httpd.c
+++ httpd.c
@@ -293,7 +293,7 @@ void execute_cgi(int client, const char *path,
             sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
             putenv(length_env);
         }
-        execl(path, NULL);
+        execl(path, path, NULL);
         exit(0);
     } else {    /* parent */
         printf("parent process, pid = %d\n", getpid());
